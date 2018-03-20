e54fc56f317f1f9f8858e613a6dea72e2d0e6573
eignatik <evg.ignatik@gmail.com>
2018-03-20 15:44:46 +0300

    Added glmatrix.js, finish drawScene method

--- computer-graphics/js/WebGLStarter.js
+++ computer-graphics/js/WebGLStarter.js
@@ -33,8 +33,7 @@ let WebGLStarter = {
             },
         };
 
-        // gl.clearColor(0.0, 0.0, 0.0, 1.0);
-        // gl.clear(gl.COLOR_BUFFER_BIT);
+        WebGLStarter.drawScene(gl, programInfo, WebGLStarter.initBuffers(gl));
     },
 
     getContext: (canvas) => {
@@ -55,7 +54,98 @@ let WebGLStarter = {
             gl,
             WebGLStarter.loadShader(gl, gl.VERTEX_SHADER, vertex),
             WebGLStarter.loadShader(gl, gl.FRAGMENT_SHADER, fragment)
-        )
+        );
+    },
+
+    drawScene: (gl, info, buffer) => {
+        // gl.clearColor(.43, .43, .43, 1.0);
+        gl.clearDepth(1.0);
+        gl.enable(gl.DEPTH_TEST);
+        gl.depthFunc(gl.LEQUAL);
+
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+        const fieldOfView = 45 * Math.PI / 180;   // in radians
+        const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+        const zNear = 0.1;
+        const zFar = 100.0;
+        const projectionMatrix = mat4.create();
+
+        mat4.perspective(projectionMatrix,
+            fieldOfView,
+            aspect,
+            zNear,
+            zFar);
+
+        const modelViewMatrix = mat4.create();
+
+        mat4.translate(
+            modelViewMatrix,
+            modelViewMatrix,
+            [-0.0, 0.0, -6.0]
+        );
+
+
+        {
+            const numComponents = 2;
+            const type = gl.FLOAT;
+            const normalize = false;
+            const stride = 0;
+            const offset = 0;
+            gl.bindBuffer(gl.ARRAY_BUFFER, buffer.position);
+            gl.vertexAttribPointer(
+                info.attribLocations.vertexPosition,
+                numComponents,
+                type,
+                normalize,
+                stride,
+                offset);
+            gl.enableVertexAttribArray(
+                info.attribLocations.vertexPosition);
+        }
+
+
+        gl.useProgram(info.program);
+
+
+        gl.uniformMatrix4fv(
+            info.uniformLocations.projectionMatrix,
+            false,
+            projectionMatrix
+        );
+        gl.uniformMatrix4fv(
+            info.uniformLocations.modelViewMatrix,
+            false,
+            modelViewMatrix
+        );
+
+        {
+            const offset = 0;
+            const vertexCount = 4;
+            gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
+        }
+    },
+
+    initBuffers: (gl) => {
+        const positionBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+        const positions = [
+            1.0, 1.0,
+            -1.0, 1.0,
+            1.0, -1.0,
+            -1.0, -1.0
+        ];
+
+        gl.bufferData(
+            gl.ARRAY_BUFFER,
+            new Float32Array(positions),
+            gl.STATIC_DRAW
+        );
+
+        return {
+            position: positionBuffer,
+        };
     },
 
     /**
