476ca55367a502c1e02b7a6941e2566a6072a939
eignatik <evg.ignatik@gmail.com>
2018-03-28 16:37:05 +0300

    Lab 3 added main class

--- javalabs/src/main/java/space/eignatik/lab3/Main.java
+++ javalabs/src/main/java/space/eignatik/lab3/Main.java
@@ -2,5 +2,7 @@ package space.eignatik.lab3;
 
 public class Main {
     public static void main(String[] args) {
+        if (args.length != 2) throw new IllegalArgumentException("Wrong arguemnts!");
+        System.out.println(args[1] + "=" + PropertyHelper.getProperty(args[0], args[1]).orElse(null));
     }
 }
